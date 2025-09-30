3faafe092167923cc2d3fc1f7742fe538b9a75ce
Sauce-Log <jhamada@berkeley.edu>
2025-09-29 22:59:13 -0700

    Cleaned up nav bar items+transitions and finished social link SVGs

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -28,15 +28,15 @@ export default function Navbar() {
           <svg
             width="48"
             height="48"
-            viewBox="0 0 24 24"
+            viewBox="0 0 504.4 504.4"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-full h-full transition-colors duration-300 fill-black hover:fill-[#c04abc]"
+            xmlns="http://www.w3.org/1999/xlink"
+            className="w-full h-full transition-colors duration-300 fill-black hover:fill-[#0072b1]"
           >
             <path
               fillRule="evenodd"
               clipRule="evenodd"
-              d="M1.15 21.7h13V61h-13zm46.55-1.3c-5.7 0-9.1 2.1-12.7 6.7v-5.4H22V61h13.1V39.7c0-4.5 2.3-8.9 7.5-8.9s8.3 4.4 8.3 8.8V61H64V38.7c0-15.5-10.5-18.3-16.3-18.3zM7.7 2.6C3.4 2.6 0 5.7 0 9.5s3.4 6.9 7.7 6.9 7.7-3.1 7.7-6.9S12 2.6 7.7 2.6z"
+              d="M377.6,0.2H126.4C56.8,0.2,0,57,0,126.6v251.6c0,69.2,56.8,126,126.4,126H378c69.6,0,126.4-56.8,126.4-126.4V126.6 C504,57,447.2,0.2,377.6,0.2z M168,408.2H96v-208h72V408.2z M131.6,168.2c-20.4,0-36.8-16.4-36.8-36.8c0-20.4,16.4-36.8,36.8-36.8 c20.4,0,36.8,16.4,36.8,36.8C168,151.8,151.6,168.2,131.6,168.2z M408.4,408.2H408h-60V307.4c0-24.4-3.2-55.6-36.4-55.6 c-34,0-39.6,26.4-39.6,54v102.4h-60v-208h56v28h1.6c8.8-16,29.2-28.4,61.2-28.4c66,0,77.6,38,77.6,94.4V408.2z"
             />
           </svg>
         </a>
@@ -52,7 +52,7 @@ export default function Navbar() {
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="w-full h-full transition-colors duration-300 fill-black hover:fill-[#c04abc]"
+            className="w-full h-full transition-colors duration-300 fill-black hover:fill-[var(--color-accent-purp)]"
           >
             <path
               fillRule="evenodd"
